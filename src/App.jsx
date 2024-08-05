961fd3174b2fb0b3ea18f7a38de5e622a380b6c6
Maximilian Hammes <maximview@gmail.com>
2024-08-05 14:36:33 +0200

    fix app.jsx and push to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import { ColorForm } from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  const handleAddColor = (newColor) => {
+    setColors([newColor, ...colors]);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <ColorForm onAddColor={handleAddColor} />
+      {colors.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
     </>
   );
 }
