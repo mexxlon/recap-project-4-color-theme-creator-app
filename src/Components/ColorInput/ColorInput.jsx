ff567f3d1705804fa8b8b3b2421d5abd6d9a36b3
Maximilian Hammes <maximview@gmail.com>
2024-08-05 12:59:48 +0200

    fixed app.jsx addcolorform.jsx colorinput.jsx

---
+++ src/Components/ColorInput/ColorInput.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function ColorInput({ id, value, onChange }) {
+  const [inputValue, setInputValue] = useState(value);
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        id={id}
+        name={id}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <input type="color" value={inputValue} onChange={handleInputChange} />
+    </>
+  );
+}
